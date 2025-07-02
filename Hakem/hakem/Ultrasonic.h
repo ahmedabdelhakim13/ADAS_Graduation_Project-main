c896e61d6ee210d49c7b36919f63cc2d5a90d876
ahmed abdelhakem <ahmedhakem801@gmail.com>
2025-07-03 01:35:00 +0300

    remove

--- Hakem/hakem/Ultrasonic.h
+++
@@ -1,9 +0,0 @@
-#ifndef _ULTRASONIC_H
-#define _ULTRASONIC_H
-
-#include <Arduino.h>
-
-void ultrasonic_init(unsigned char triggerPin, unsigned char echoPin);
-unsigned long ultrasonic_Get_Distance(unsigned char triggerPin, unsigned char echoPin);
-
-#endif
